4d51f5cf61ced66d14404e6a3e2829a6cd6fa13f
sjahn <asj214@naver.com>
2019-12-24 17:47:34 +0900

    초기 셋팅 작업

---
+++ database/migrations/2019_12_24_080204_add_columns_in_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnsInUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(){
+
+        Schema::table('users', function (Blueprint $table){
+            $table->integer('level')->default(1)->after('password'); // 사용자 권한
+            $table->datetime('last_login_at')->nullable()->after('remember_token'); // 최근 접속 일
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
