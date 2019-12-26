2addf0051c5af6378d4d879d72daf2cd538b449e
sjahn <asj214@naver.com>
2019-12-26 10:25:30 +0900

    appServiceProvider.php database query log 기능 추가

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,26 +2,39 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Events\QueryExecuted;
 
 class AppServiceProvider extends ServiceProvider {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
+
+    public function register(){
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
+    public function boot(){
+
+        // database query logging
+        DB::listen(function($query){
+
+            foreach($query->bindings as $i => $binding){
+                if($binding instanceof \DateTime){
+                    $query->bindings[$i] = $binding->format('\'Y-m-d H:i:s\'');
+                } else {
+                    if(is_string($binding)){
+                        $query->bindings[$i] = "'$binding'";
+                    }
+                }
+            }
+
+            // Insert bindings into query
+            $boundSql = str_replace(['%', '?'], ['%%', '%s'], $query->sql);
+            $boundSql = vsprintf($boundSql, $query->bindings);
+
+            Log::info($boundSql." (".$query->time."s)");
+
+        });
+
     }
 }
