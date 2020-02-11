1a7be581fd84b0f18beb124cf5fc51ac9007b503
안성주 <ahnsb214@gmail.com>
2020-02-11 14:49:59 +0900

    query log 분리

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,30 +11,31 @@ class AppServiceProvider extends ServiceProvider {
 
     public function register(){
         //
+        if($this->app->environment() != 'production'){
+
+            \Event::listen('Illuminate\Database\Events\QueryExecuted', function ($query) {
+
+                foreach($query->bindings as $i => $binding){
+                    if($binding instanceof \DateTime){
+                        $query->bindings[$i] = $binding->format('\'Y-m-d H:i:s\'');
+                    } else {
+                        if(is_string($binding)){
+                            $query->bindings[$i] = "'$binding'";
+                        }
+                    }
+                }
+
+                $real_query = str_replace(['%', '?'], ['%%', '%s'], $query->sql);
+                $real_query = vsprintf($real_query, $query->bindings);
+
+                Log::channel('sql')->info($real_query." (".$query->time."s)");
+
+            });
+
+        }
+
     }
 
     public function boot(){
-
-        // database query logging
-        DB::listen(function($query){
-
-            foreach($query->bindings as $i => $binding){
-                if($binding instanceof \DateTime){
-                    $query->bindings[$i] = $binding->format('\'Y-m-d H:i:s\'');
-                } else {
-                    if(is_string($binding)){
-                        $query->bindings[$i] = "'$binding'";
-                    }
-                }
-            }
-
-            // Insert bindings into query
-            $boundSql = str_replace(['%', '?'], ['%%', '%s'], $query->sql);
-            $boundSql = vsprintf($boundSql, $query->bindings);
-
-            Log::info($boundSql." (".$query->time."s)");
-
-        });
-
     }
 }
